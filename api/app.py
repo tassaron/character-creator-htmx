c4064d1357f59bba8bd2e84e5cad649b2fc347fc
Brianna Rainey <brianna@tassaron.com>
2023-07-30 21:03:37 -0400

    update image in esponse to form

--- api/app.py
+++ api/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, Blueprint, render_template, url_for, request
-from dnd_character import Character, CLASSES
+from dnd_character import Character, CLASSES, __version__
 from . import character
 
 
@@ -14,28 +14,34 @@ def create_app() -> Flask:
 
 @main.route("/")
 def index() -> str:
-    return render_template("index.html")
+    return render_template("index.html", dep_version=__version__)
 
 
 @main.route("/data", methods=["POST"])
 def get_data() -> str:
+    if request.form["class"]:
+        classs = CLASSES[str(request.form["class"])]
+    else:
+        classs = None
     return render_template(
         "data.html",
         character_data=dict(
             Character(
                 name=str(request.form["name"]),
                 level=int(request.form["level"]),
-                classs=CLASSES[str(request.form["class"])],
+                classs=classs,
             )
         ),
+        dep_version=__version__,
     )
 
 
-@main.route("/image")
+@main.route("/image", methods=["POST"])
 def get_image() -> str:
+    keys = ["body", "head", "face", "hair", "hat"]
     filename = character.get_character_image_filename(
-        "0", "headcircle", "faceneutral", "hairnancy", "hatwinter"
+        *[f"{key if key != 'body' else ''}{request.form[key]}" for key in keys]
     )
     filename = url_for("static", filename=f"img/character/{filename}")
     desc = "this character"
-    return f"<div><img src='{filename}' alt='{desc}'></div>"
+    return f"<div id='character-image'><img src='{filename}' alt='{desc}'></div>"
